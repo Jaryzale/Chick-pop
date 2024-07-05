81646c2dcaecb9d510e0eb95c2158a81d750a3eb
Jaryzale <jaryzale803@gmail.com>
2024-07-05 16:58:20 +0800

    Add files via upload

---
+++ Chick Pop/fooditem.js
@@ -0,0 +1,332 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Snow Cheese',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Snow Cheese Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Salted Egg',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Salted Egg Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Garlic Parmesan',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Garlic Parmesan Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Soy Garlic',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Soy Garlic Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Lemon Glazed',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Lemon Glazed Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Sweet & Sour',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Sweet & Sour Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Teriyaki',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Teriyaki Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Honey Garlic',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Honey Garlic Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Buffalo',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Buffalo Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Hickory BBQ',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Hickory BBQ Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Spicy Korean BBQ',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Spicy Korean BBQ Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Sweet Chili',
+    category : 'chickpop',
+    rating : 4.3,
+    price: 59,
+    img: 'images/chickpop/Sweet Chili Chick-Pop.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Classic',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/classic.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Thai',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/thai.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Honey',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/honey.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Honey Dew',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/honeydew.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Mango',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/mango.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Peach',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/peach.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Taro',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/Taro.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Matcha',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/macha.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Guava',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/guava.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Passion Fruit',
+    age: '32',
+    category : 'milktea',
+    rating : 4.3,
+    price: 29,
+    img: 'images/milk-tea/passion fruit.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Vanilla',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/vanilla.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Chocolate',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/choco.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Coffee',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/coffee.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Rose',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/rose.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Coconut',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/coconut.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Earl Grey',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/earl.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Spicy Chai',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/spicy-chai.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Hazelnut',
+    category : 'tea',
+    rating : 4.3,
+    price: 39,
+    img: 'images/special-tea/hazelnut.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: '12pcs Ala Carte',
+    category : 'offer',
+    rating : 4.3,
+    price: 119,
+    img: 'images/offering/ala-carte.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: '20pcs Ala Carte',
+    category : 'offer',
+    rating : 4.3,
+    price: 159,
+    img: 'images/offering/ala-carte.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Extra Rice',
+    category : 'offer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/offering/rice.jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Sulit Meal',
+    category : 'offer',
+    rating : 4.3,
+    price: 59,
+    img: 'images/offering/platter.jpeg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Busog Meal',
+    category : 'offer',
+    rating : 4.3,
+    price: 79,
+    img: 'images/offering/platter.jpeg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Swak Meal',
+    category : 'offer',
+    rating : 4.3,
+    price: 99,
+    img: 'images/offering/platter.jpeg',
+    quantity: 1
+},
+]
+
+export {foodItem};
